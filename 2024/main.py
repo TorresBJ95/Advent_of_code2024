463601e9447f3c018ace1882352d62a8fa57b5ca
Brayan Torres <btorres@fi.uba.ar>
2024-12-02 23:23:21 -0300

    Second problem advent of code

--- 2024/main.py
+++ 2024/main.py
@@ -1,4 +1,5 @@
 from Problem_1 import comp_two_number_list
+from Problem_2 import numbers_diference
 import os
 
 def send_txt(txt_paths: list):
@@ -17,11 +18,16 @@ if __name__ == '__main__':
     files = os.listdir(path)
     txt_path = list()
     for file in files:
-        if file.endswith(".txt"):
-            to_save = os.path.join(path, file)
-            txt_path.append(to_save)
+        if file == 'inputs':
+            folder_inputs = os.path.join(path, file)
+            txts = os.listdir(folder_inputs)
+            for txt in txts:
+                to_save = os.path.join(folder_inputs, txt)
+                txt_path.append(to_save)
+            break
 
     file_path = send_txt(txt_path)
-    comp_two_number_list(file_path)
+    #comp_two_number_list(file_path)
+    numbers_diference(file_path)
 
 
